79b3faae7b9767acf518e1759a91a98e04aec229
ashwinibailangadi <ashwinib40@gmail.com>
2023-10-21 21:56:29 -0700

    refactoring changes

--- octokit-app/routes/commits.js
+++ octokit-app/routes/commits.js
@@ -6,7 +6,14 @@ router.get('/:owner/:repo', async (req, res) => {
   const { owner, repo } = req.params;
   try {
     const { data: commits } = await octokit.repos.listCommits({ owner, repo });
-    res.json(commits);
+  // Extract only the desired fields from each commit
+  const filteredCommits = commits.map((commit) => ({
+    sha: commit.sha,
+    author: commit.commit.author.name,
+    date: commit.commit.author.date,
+    message: commit.commit.message,
+  }));
+    res.json(filteredCommits);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
