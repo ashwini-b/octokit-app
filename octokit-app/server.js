76543855dc0d526d1b7d4aab7afaa5f67190360a
ashwinibailangadi <ashwinib40@gmail.com>
2023-10-22 18:51:15 -0700

    createPRandgetPR

--- octokit-app/server.js
+++ octokit-app/server.js
@@ -3,6 +3,8 @@ const authRouter = require('./routes/auth');
 const commitsRouter = require('./routes/commits');
 const getContentsRouter = require('./routes/getcontents');
 const createCommitRouter = require('./routes/createCommit');
+const createPullRequestsRouter = require('./routes/createPullRequests');
+const getPullRequestsRouter = require('./routes/getPullRequests');
 const app = express();
 
 // Middleware for parsing JSON request bodies
@@ -13,6 +15,8 @@ app.use('/api/auth', authRouter);
 app.use('/api/commits', commitsRouter);
 app.use('/api/contents',getContentsRouter);
 app.use('/api/crtcommit',createCommitRouter);
+app.use('/api/createpr',createPullRequestsRouter);
+app.use('/api/getpr', getPullRequestsRouter);
 // Start the server
 const port = 3000;
 app.listen(port, () => {
