dcf593ba2408ada611a7c1273df57574145a7ae9
GowthamReddyIV <148672110+GowthamReddyIV@users.noreply.github.com>
2023-10-23 18:12:31 -0700

    Update createbranch.js

--- octokit-app/routes/createbranch.js
+++ octokit-app/routes/createbranch.js
@@ -1,16 +1,13 @@
 const express = require('express');
 const octokit = require('../github'); // Import Octokit
 const router = express.Router();
-
-// Route to create a new branch in a GitHub repository
-router.get('/create/:owner/:repo/:branchName', async (req, res) => {
+router.get('/create/:owner/:repo/:branchName', async (req, res) => {      // Route to create a new branch in a GitHub repository
     const {owner,repo,branchName} = req.params;
     const commits = await octokit.repos.listCommits({
             owner,
             repo
     });
-    
-        const latestCommit = commits.data[0].sha;
+           const latestCommit = commits.data[0].sha;
     const response = await octokit.git.createRef({
         owner,
         repo,
@@ -20,5 +17,4 @@ router.get('/create/:owner/:repo/:branchName', async (req, res) => {
       console.log(response);
       res.json("SuccessFull");
 });
-
 module.exports = router;
